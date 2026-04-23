3e078b8ee50866af62aaf96aa2ff67774ca812d2
Mohd-Umair-Lari <umairlari0786@gmail.com>
2026-04-24 00:01:03 +0530

    Completed tye code with much better approach

--- Data Structures & Algorithms/find-duplicate-integer/submission-6.py
+++ Data Structures & Algorithms/find-duplicate-integer/submission-6.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
       n = len(nums)
@@ -9,8 +12,20 @@ class Solution:
         i += 1
          
 
-
+# much optimised approach
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        n = len(nums)
+        res=0
+        l=[]
+        for i in range(n):
+            if(nums[i] not in l):
+                l.append(nums[i])
+            else:
+                res=nums[i]
+        return res
+    
         
-        
+# If the question asks for the duplicate number without modifying the array and requires indexing do let me know
     
             
